c9cce958d89454a5c9ac1c75771bdc5111077de8
Awatansh <aw-strakh@Awatanshs-MacBook-Pro.local>
2023-12-02 18:30:36 +0530

    Multi threaded task scheduler

---
+++ src/main/java/org/example/TaskStore.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public interface TaskStore<T extends ScheduledTask> {
+
+    T peek();
+
+    T poll();
+
+    void add(T task);
+
+    boolean remove(T task);
+
+    boolean isEmpty();
+}
